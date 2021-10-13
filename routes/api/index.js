8a333ee5d3a7b2571e3376d35770a1067eb77ddd
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-13 09:30:49 -0600

    added CRUD post routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@
 
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 // this file takes the routes from user-routes and implements them to another router instance, prefixing them with the path /users at that time
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 
 
