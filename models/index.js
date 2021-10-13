3c87fb314b6fc813df9c3582de5809ea2e0c439a
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-12 18:58:43 -0600

    started creating routes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,17 @@ const User = require('./User');
 const Post = require('./Post');
 
 
+// creates a link between the id column in User to the corresponding foreign key pair which is the user_id in the Post model
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+// making the reverse association (defining the relationship of the Post model to the User model || contraint imposed is a post can belong to one user, not many users || link is declared to the foreign key user_id in the Post model
+Post.belongsTo(User, {
+   foreignKey: 'user_id',
+});
+
+
 
 
 
