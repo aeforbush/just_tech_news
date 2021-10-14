0dfaa8bdc68fcf2b48c86a3b3da9c396bb3de448
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-14 16:54:51 -0600

    created Vote model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 
 // creates a link between the id column in User to the corresponding foreign key pair which is the user_id in the Post model
@@ -12,11 +13,38 @@ Post.belongsTo(User, {
    foreignKey: 'user_id',
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
 
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
 
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+    
 
 
 
 
 // exporting an object with User as a property | created class User (new model) to inherit the functionality of the Model class ( CRUD )
-module.exports = {User, Post};
+module.exports = {User, Post, Vote};
