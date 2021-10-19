5272c6df1c93768dde62afbe9c05fc28fbd3914c
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-19 11:05:01 -0600

    rendered login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
+
 router.get("/", (req, res) => {
   Post.findAll({
     attributes: [
@@ -48,5 +49,11 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+
 
 module.exports = router;
